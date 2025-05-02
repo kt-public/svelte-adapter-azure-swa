31b5413d96ff7b07536a199f5efdfdc69b23c41e
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-05-02 18:12:58 +0200

    remove old demo app

--- tests/demo-old/src/routes/+page.js
+++
@@ -1,8 +0,0 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = !process.env.SWA; // swa at the moment has issue with serving prerendered assets
-
-console.warn('#'.repeat(100));
-console.warn(`SWA: ${process.env.SWA}`);
-console.warn(`prerender: ${prerender}`);
-console.warn('#'.repeat(100));
