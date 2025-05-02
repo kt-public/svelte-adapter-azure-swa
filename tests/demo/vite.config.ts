ededeecfdf205a25c377553ead7b0c6770cfc55e
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-05-02 23:19:12 +0200

    For Sentry bug

--- tests/demo/vite.config.ts
+++ tests/demo/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	},
 	plugins: [
 		sentrySvelteKit({
-			adapter: 'other',
+			adapter: process.env.SWA ? 'other' : 'node',
 			sourceMapsUploadOptions: {
 				org: 'konstantin-tarmyshov',
 				project: 'svelte-adapter-azure-swa',
