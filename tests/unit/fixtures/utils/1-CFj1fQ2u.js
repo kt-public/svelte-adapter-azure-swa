423aee93cae799f56ee5b258a875e09ef4e4361a
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-05-07 09:59:45 +0200

    rewrite source for sentry

--- tests/unit/fixtures/utils/1-CFj1fQ2u.js
+++ tests/unit/fixtures/utils/1-CFj1fQ2u.js
@@ -1,7 +1,19 @@
 const index = 1;
 let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-C5oCqPqZ.js')).default;
-const imports = ["_app/immutable/nodes/1.LilAoUXF.js","_app/immutable/chunks/DUclCSqf.js","_app/immutable/chunks/ip5fKZKC.js","_app/immutable/chunks/KaRZCzPz.js","_app/immutable/chunks/BF4L8yDd.js","_app/immutable/chunks/DdWqggKP.js","_app/immutable/chunks/DFUf_SXc.js","_app/immutable/chunks/CSCqRXKJ.js","_app/immutable/chunks/LTNcka-5.js","_app/immutable/chunks/Dn2m_cjW.js"];
+const component = async () =>
+	(component_cache ??= (await import('./error.svelte-C5oCqPqZ.js')).default);
+const imports = [
+	'_app/immutable/nodes/1.LilAoUXF.js',
+	'_app/immutable/chunks/DUclCSqf.js',
+	'_app/immutable/chunks/ip5fKZKC.js',
+	'_app/immutable/chunks/KaRZCzPz.js',
+	'_app/immutable/chunks/BF4L8yDd.js',
+	'_app/immutable/chunks/DdWqggKP.js',
+	'_app/immutable/chunks/DFUf_SXc.js',
+	'_app/immutable/chunks/CSCqRXKJ.js',
+	'_app/immutable/chunks/LTNcka-5.js',
+	'_app/immutable/chunks/Dn2m_cjW.js'
+];
 const stylesheets = [];
 const fonts = [];
 
