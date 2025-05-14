92ad682b41a8c831a41575130c159c33cac02861
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-05-14 23:06:39 +0200

    @azure/cosmos

--- tests/demo/src/routes/sentry-example-page/+server.js
+++ tests/demo/src/routes/sentry-example-page/+server.js
@@ -1,6 +1,10 @@
 // This is just a very simple API route that throws an example error.
 // Feel free to delete this file and the entire sentry route.
 
+import { CosmosClient } from '@azure/cosmos';
+
 export const GET = async () => {
+	const cosmosdb = new CosmosClient({});
+	console.log(cosmosdb);
 	throw new Error('Sentry Example API Route Error');
 };
