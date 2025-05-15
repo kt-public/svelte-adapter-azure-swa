0830b5f0af05998bdc1f327a737562d62ef8732b
Konstantin Tarmyshov <67151862+ktarmyshov@users.noreply.github.com>
2025-05-15 18:49:58 +0200

    @Azure/Cosmos issue (#104)
    * @azure/cosmos
    
    * update

--- tests/demo/src/routes/sentry-example-page/+server.js
+++ tests/demo/src/routes/sentry-example-page/+server.js
@@ -1,6 +1,18 @@
 // This is just a very simple API route that throws an example error.
 // Feel free to delete this file and the entire sentry route.
 
-export const GET = async () => {
+import { CosmosClient } from '@azure/cosmos';
+
+export const GET = async (event) => {
+	event.platform?.context.log('Sentry Example API Route');
+	try {
+		event.platform?.context.warn(
+			'Trying to create CosmosClient, should fail, cause we provide no config'
+		);
+		const cosmosClient = new CosmosClient({});
+		console.log('CosmosClient created', cosmosClient);
+	} catch (error) {
+		event.platform?.context.error('Error creating CosmosClient', error);
+	}
 	throw new Error('Sentry Example API Route Error');
 };
